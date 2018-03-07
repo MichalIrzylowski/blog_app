ec5ab30c4cadbcd3d2df36f9ffc30ec63f1e7f28
Michał <michal.irzylowski@gmail.com>
2018-03-07 18:03:58 +0000

    add public/stylesheets/landing/style.css add routes/blog.js add views/home.ejs

--- routes/blog.js
+++ routes/blog.js
@@ -5,7 +5,7 @@ const   express = require("express"),
 
 //INDEX ROUTE
 router.get("/", function(req, res) {
-    res.redirect("/blog");
+    res.render("home");
 });
 
 router.get("/blog", function(req, res){
